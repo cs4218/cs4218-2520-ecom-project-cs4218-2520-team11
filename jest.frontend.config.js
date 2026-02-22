407f3a61d2cbdb8975bbe083dd850d6720feecab
gambe <67666355+juliusgambe@users.noreply.github.com>
2026-02-22 21:00:28 +0800

    Update jest.frontend.config.js

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -20,7 +20,10 @@ export default {
   transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
 
   // only run these tests
-  testMatch: ["<rootDir>/client/src/pages/**/*.test.js"],
+  testMatch: [
+    "<rootDir>/client/src/pages/**/*.test.js",
+    "<rootDir>/client/src/components/**/*.test.js",
+  ],
 
   // jest code coverage
   collectCoverage: true,
