636e929b3965124296d7b4bb8ed6a0357a1c531b
Gabriel Seethor Jun Hao <gabriel@Gabriels-MacBook-Air-2.local>
2026-02-22 16:57:50 +0800

    Change config

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -13,8 +13,8 @@ export default {
   collectCoverageFrom: ["controllers/**"],
   coverageThreshold: {
     global: {
-      lines: 100,
-      functions: 100,
+      lines: 80,
+      functions: 80,
     },
   },
 };
--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -27,8 +27,8 @@ export default {
   collectCoverageFrom: ["client/src/pages/Auth/**"],
   coverageThreshold: {
     global: {
-      lines: 100,
-      functions: 100,
+      lines: 80,
+      functions: 80,
     },
   },
   setupFilesAfterEnv: ["<rootDir>/client/src/setupTests.js"],
